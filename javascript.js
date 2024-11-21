2f907cd98424bd155764677cf404f9d7ed97e41f
Daveian <[dguxdesigns@gmail.com]>
2024-11-20 22:30:44 -0500

    Add function to close dialog when clicking outside of it
    - Enhances user experience by allowing easy closing of the modal without a close button

--- javascript.js
+++ javascript.js
@@ -34,7 +34,17 @@ addButton.addEventListener("click", () => {
   dialog.showModal();
 })
 
-
+dialog.addEventListener('click', e => {
+  const dialogDimensions = dialog.getBoundingClientRect()
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    dialog.close();
+  };
+})
 
 
 
