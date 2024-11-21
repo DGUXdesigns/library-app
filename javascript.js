59be05ca69ee2c7642067b5c932e2b2f1651365a
Daveian <[dguxdesigns@gmail.com]>
2024-11-20 22:13:58 -0500

    Implement dialog modal for form submission.
    - Added <dialog> element for modal display.
    - Implemented JavaScript to open modal when 'Add' button is clicked.
    - Styled modal with fade-in animation and backdrop effect.
    - Adjusted form layout for modal use.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,13 @@
-const myLibrary = ["Dandadan", "Sakamoto Days", "Kagurabachi", "Blue Lock", "One Piece", "Chainsaw Man"];
+const myLibrary = [
+  "Dandadan", 
+  "Sakamoto Days", 
+  "Kagurabachi", 
+  "Blue Lock", 
+  "One Piece", 
+  "Chainsaw Man"
+];
+const addButton = document.querySelector("#showDialog");
+const dialog = document.querySelector("dialog");
 
 function Book() {
   // the constructor...
@@ -13,10 +22,21 @@ function displayBooks(myLibrary) {
 
   myLibrary.forEach(book => {
     const info = document.createElement('div');
+    info.classList.add("book-card")
     info.textContent = book;
 
     container.appendChild(info);
   });
 }
+
+//Initialize buttons
+addButton.addEventListener("click", () => {
+  dialog.showModal();
+})
+
+
+
+
+
 
 displayBooks(myLibrary);
